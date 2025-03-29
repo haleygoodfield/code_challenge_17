08f49143c3a17645c7511e98fd7590a8d3fa713b
Haley Goodfield <haleygoodfield@usf.edu>
2025-03-29 16:44:36 -0400

    Task 2 – Created SalesRep class

--- cc_17.js
+++ cc_17.js
@@ -5,16 +5,16 @@ class Customer { // Create the customer class
     email = ''; // email (string)
     purchaseHistory = []; // purchaseHistory (array)
 
-    addPurchase(amount) {
+    addPurchase(amount) { // create method addPurchase(amount)
         this.purchaseHistory.push(amount); // adds a purchase amount to purchaseHistory
     }
 
-    getTotalSpent() {
+    getTotalSpent() { // create method getTotalSpent()
         return this.purchaseHistory.reduce((total, amount) => total + amount, 0); // returns total amount spent
     }
 }
 
-// Creating new customer creation and total spent after purchases
+// Log: New customer creation and total spent after purchases.
 const newCustomer = new Customer(); // create a new customer
 newCustomer.name = 'Noelle'; // assign name
 newCustomer.email = 'noellef@gmail.com'; // assign email
@@ -24,6 +24,32 @@ newCustomer.addPurchase(100); // assign another purchase
 console.log(`Customer Name: ${newCustomer.name}`); // logs the new customer's name
 console.log(`Customer Email: ${newCustomer.email}`); // logs the new customer's email
 console.log(`Total Spent: $${newCustomer.getTotalSpent()}`); // logs their total spent 
+
+// Task 2: Create a SalesRep Class
+class SalesRep{ // Create the SalesRep class  
+    name = ''; // name (string)
+    clients = []; // clients (array of Customer objects)
+
+    addClient(customer) { // create method addClient(customer) 
+        this.clients.push(customer); // adds a customer to the list
+    }
+
+    getClientTotal(name) { // create method getClientTotal(name)
+        const client = this.clients.find(client => client.name === name); // finds client by name
+        return client ? client.getTotalSpent() : 0; // returns the total spent 
+    }
+}
+const ClientName = newSalesRep.clients.map(client => client.name);
+
+// Log: Sales rep’s clients and total spent for a specific client.
+const newSalesRep = new SalesRep(); // create a new SalesRep
+newSalesRep.name = 'Marie'; // assign name
+newSalesRep.addClient(newCustomer);
+console.log(`Sales Rep: ${newSalesRep.name}`)
+console.log(`Sales Rep Clients: ${ClientName}`)
+console.log(`Total Spent by Client: ${newSalesRep.getClientTotal('Noelle')}`);
+
+
 
 
 
